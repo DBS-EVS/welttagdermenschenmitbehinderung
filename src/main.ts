156c07da9059d313c7da480e6db7ad5ef1f65ba8
AgniKai95 <97593111+AgniKai95@users.noreply.github.com>
2023-11-03 15:51:22 +0100

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,165 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import { bootstrapExtra } from "@workadventure/scripting-api-extra"
 
-console.log('Script started successfully');
+console.log('Script started successfully')
 
-let currentPopup: any = undefined;
+// Tours layers
+const DB_SCHENKER_HQ = 'DB_Schenker_HQ'
+const DB_CONNECT = 'DB_Connect'
+const DB_SCHENKER_LOGISTICS = 'DB_Schenker_Logistics'
+const BERLIN_MAIN_STATION = 'Berlin_Main_Station'
+const BAHNTOWER = 'Bahntower'
+const SILVER_TOWER = 'Silver_Tower'
+
+let currentLevel = 0
+let playerOnStairs = false
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
-    console.log('Scripting API ready');
+    console.log('Scripting API ready')
     console.log('Player tags: ',WA.player.tags)
-
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
-
-    WA.room.area.onLeave('clock').subscribe(closePopup)
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
-        console.log('Scripting API Extra ready');
-    }).catch(e => console.error(e));
+        console.log('Scripting API Extra ready')
 
-}).catch(e => console.error(e));
+        if (WA.state.currentMap === 'city') {
+            listenTour(DB_SCHENKER_HQ)
+            listenTour(DB_CONNECT)
+            listenTour(DB_SCHENKER_LOGISTICS)
+            listenTour(BERLIN_MAIN_STATION)
+            listenTour(BAHNTOWER)
+            listenTour(SILVER_TOWER)
+        }
+    
+        if (WA.state.currentMap === 'station') {
+            // for now, we have to disable WebRTC when players on different levels can be on the same 2D position.
+            WA.room.onEnterLayer("webrtc").subscribe(() => {
+                WA.controls.restorePlayerProximityMeeting()
+            })
+            WA.room.onLeaveLayer("webrtc").subscribe(() => {
+                WA.controls.disablePlayerProximityMeeting()
+            })
 
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
+            // We are on level0 by default
+            hideLevel(1)
+            hideLevel(-1)
+    
+            listenStairs()
+    
+            // Switch levels
+            listenLevel(1)
+            listenLevel(0)
+            listenLevel(-1)
+    
+            // Hide roofs when player walk below them
+            WA.room.onEnterLayer(`level${currentLevel}/above/roof1`).subscribe(() => {
+                hideMultipleLayers([
+                    `level${currentLevel}/above/roof3`,
+                    `level${currentLevel}/above/roof2`,
+                    `level${currentLevel}/above/roof1`
+                ])
+            })
+            WA.room.onLeaveLayer(`level${currentLevel}/above/roof1`).subscribe(() => {
+                showMultipleLayers([
+                    `level${currentLevel}/above/roof3`,
+                    `level${currentLevel}/above/roof2`,
+                    `level${currentLevel}/above/roof1`
+                ])
+            })
+        }    
+    }).catch(e => console.error(e))
+}).catch(e => console.error(e))
+
+const listenTour = (tour: string) => {
+    WA.room.area.onEnter('Behind_' + tour).subscribe(() => {
+        WA.room.hideLayer('above/' + tour)
+    })
+    WA.room.area.onLeave('Behind_' + tour).subscribe(() => {
+        WA.room.showLayer('above/' + tour)
+    })
+}
+
+const listenStairs = () => {
+    WA.room.area.onEnter('stairs').subscribe(() => {
+        playerOnStairs = true
+    })
+    WA.room.area.onLeave('stairs').subscribe(() => {
+        playerOnStairs = false
+    })
+}
+
+const listenLevel = (level: number) => {
+    WA.room.onEnterLayer(`level${level}/floor/floor1`).subscribe(() => {
+        if (playerOnStairs) {
+            // show the level we are on
+            currentLevel = level
+            showLevel(level)
+
+            // hide all levels except the current one
+            const levels: number[] = [-1, 0, 1]
+            const levelsToHide = levels.filter(l => l !== level)
+            console.log('levelsToHide',levelsToHide)
+            for (let level of levelsToHide) {
+                hideLevel(level)
+            }
+        }
+    })
+}
+
+const showLevel = (level: number) => {
+    showMultipleLayers([
+        `level${level}/above/above3`,
+        `level${level}/above/above2`,
+        `level${level}/above/above1`,
+        `level${level}/above/roof3`,
+        `level${level}/above/roof2`,
+        `level${level}/above/roof1`,
+        `level${level}/furniture/furniture3`,
+        `level${level}/furniture/furniture2`,
+        `level${level}/furniture/furniture1`,
+        `level${level}/walls/walls2`,
+        `level${level}/walls/walls1`,
+        `level${level}/floor/floor2`,
+        `level${level}/floor/floor1`,
+        `level${level}/collisions`
+    ])
+}
+const hideLevel = (level: number) => {
+    hideMultipleLayers([
+        `level${level}/collisions`,
+        `level${level}/floor/floor1`,
+        `level${level}/floor/floor2`,
+        `level${level}/walls/walls1`,
+        `level${level}/walls/walls2`,
+        `level${level}/furniture/furniture1`,
+        `level${level}/furniture/furniture2`,
+        `level${level}/furniture/furniture3`,
+        `level${level}/above/roof1`,
+        `level${level}/above/roof2`,
+        `level${level}/above/roof3`,
+        `level${level}/above/above1`,
+        `level${level}/above/above2`,
+        `level${level}/above/above3`
+    ])
+}
+
+const showMultipleLayers = (layers: string[]) => {
+    console.log('-----------------------------')
+    console.log('currentLevel',currentLevel)
+    for(let layer of layers) {
+        console.log("SHOW layer", layer)
+        WA.room.showLayer(layer)
+    }
+}
+const hideMultipleLayers = (layers: string[]) => {
+    console.log('-----------------------------')
+    console.log('currentLevel',currentLevel)
+    for(let layer of layers) {
+        console.log("HIDE layer", layer)
+        WA.room.hideLayer(layer)
     }
 }
 
-export {};
+export {}
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,5 @@
-import 'dotenv/config';
 import { defineConfig } from "vite";
-import { getMaps, getMapsOptimizers, getMapsScripts, LogLevel, OptimizeOptions } from "wa-map-optimizer-vite";
-
-const maps = getMaps();
-
-let optimizerOptions: OptimizeOptions = {
-    logs: process.env.LOG_LEVEL && process.env.LOG_LEVEL in LogLevel ? LogLevel[process.env.LOG_LEVEL] : LogLevel.NORMAL,
-};
-
-if (process.env.TILESET_OPTIMIZATION && process.env.TILESET_OPTIMIZATION === "true") {
-    const qualityMin = process.env.TILESET_OPTIMIZATION_QUALITY_MIN ? parseInt(process.env.TILESET_OPTIMIZATION_QUALITY_MIN) : 0.9;
-    const qualityMax = process.env.TILESET_OPTIMIZATION_QUALITY_MAX ? parseInt(process.env.TILESET_OPTIMIZATION_QUALITY_MAX) : 1;
-
-    optimizerOptions.output = {
-        tileset: {
-            compress: {
-                quality: [qualityMin, qualityMax],
-            }
-        }
-    }
-}
+import { getMapsOptimizers, getMapsScripts } from "wa-map-optimizer-vite";
 
 export default defineConfig({
     base: "./",
@@ -27,11 +7,11 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 index: "./index.html",
-                ...getMapsScripts(maps),
+                ...getMapsScripts("./maps"),
             },
         },
     },
-    plugins: [...getMapsOptimizers(maps, optimizerOptions)],
+    plugins: [...getMapsOptimizers(undefined, "./maps")],
     server: {
         host: "localhost",
         headers: {
