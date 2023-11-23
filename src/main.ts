671dc5474b98170e6858c67161958995923eca99
AgniKai95 <97593111+AgniKai95@users.noreply.github.com>
2023-11-23 16:12:00 +0100

    skript

--- src/main.ts
+++ src/main.ts
@@ -22,26 +22,23 @@ let currentPopup: any = undefined;
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready')
-    console.log('Player tags: ',WA.player.tags)
+    console.log('Player tags: ',WA.player.tags
+
+    currentPopup =  WA.ui.openPopup(popUpStart, startMsg,[
+        {
+            label: "OK",
+            callback: (popup) => {
+                popup.close();
+                currentPopup = undefined;
+            }
+        }]
+    );
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready')
 
         
-
-        currentPopup =  WA.ui.openPopup(popUpStart, startMsg,[
-            {
-                label: "OK",
-                callback: (popup) => {
-                    popup.close();
-                    currentPopup = undefined;
-                }
-            }]
-        );     
-
-
-
 
         if (WA.state.currentMap === 'city') {
             listenTour(DB_SCHENKER_HQ)
